f09b04e4e2655b56dfe11dfe75cd9b112799f182
thenatespack <natespack@icloud.com>
2025-08-25 14:47:43 -0600

    navbar

--- forum.php
+++ forum.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Neumont Forums</title>
+    <title>Document</title>
 </head>
 <body>
-    <?php include 'navbar.php';?>
-    <h1>Home</h1>
+    <?php include 'navbar.php'; ?>
+    <h1>Forum</h1>
 </body>
 </html>
--- navbar.php
+++ navbar.php
@@ -3,7 +3,7 @@ $is_logged_in =false;
 ?>
 <navbar>
     <a href="Home">Home</a>
-    <a href="Forum">Forum</a>
+    <a href="forum.php">Forum</a>
     <a href="Your Posts">Your Posts</a>
     <?php echo  $is_logged_in ?'': '<a href="Log In"></a>' ?>
 </navbar>
