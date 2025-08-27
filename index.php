7cd7575f104d3a8b428caa85a2388e15765eacfa
JBrincefield <jbrincefield@student.neumont.edu>
2025-08-27 15:27:45 -0600

    added icon to navbar
    added an images folder to public added the icon to the navbar

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Neumont Forums</title>
 </head>
+
 <body>
-    <?php include 'navbar.php';?>
+    <?php include 'navbar.php'; ?>
     <h1>Home</h1>
-    This is a Fucken Test I want to Die
 </body>
+
 </html>
--- navbar.php
+++ navbar.php
@@ -1,10 +1,11 @@
 <?php
-$is_logged_in =false;
+$is_logged_in = false;
 ?>
 <link rel="stylesheet" type="text/css" href="public\site.css">
 <navbar class="nav-inner">
+    <image src="public/images/forum_icon.png" alt="Neumont Forums Logo" height="20px" width="20px"></image>
     <a href="index.php">Home</a>
     <a href="forum.php">Forum</a>
     <a href="Your Posts">Your Posts</a>
-    <?php echo  $is_logged_in ?'': '<a href="Log In"></a>' ?>
+    <?php echo $is_logged_in ? '' : '<a href="Log In"></a>' ?>
 </navbar>
